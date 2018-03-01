6ae97b994a8ead53ed8293ea06b6176fdcabfe13
preethi <chime006@d.umn.edu>
2018-02-28 22:11:39 -0800

    Setup MVC groups, setup Books list table view controller, created custom Book cell

---
+++ BookStorePro/BookStorePro/Controller/BooksViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BookStorePro
+//
+//  Created by preethi Reddy on 2/28/18.
+//  Copyright © 2018 com.preethichimerla. All rights reserved.
+//
+
+import UIKit
+
+class BooksViewController: UIViewController{
+
+    @IBOutlet weak var booksTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        booksTableView.delegate = self
+        booksTableView.dataSource = self
+    }
+}
+
+extension BooksViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return nil
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+    }
+}
+
---
+++ BookStorePro/BookStorePro/View/BookCell.swift
@@ -0,0 +1,21 @@
+//
+//  BookCell.swift
+//  BookStorePro
+//
+//  Created by preethi Reddy on 2/28/18.
+//  Copyright © 2018 com.preethichimerla. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var authorsLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
